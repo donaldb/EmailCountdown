e666915b907e3c64b48b206d28a27e1033db03b3
donaldb <don.braithwaite@outlook.com>
2017-02-16 23:04:22 -0500

    mods not working

--- gif.php
+++ gif.php
@@ -1,55 +1,65 @@
 <?php
 
-	//Leave all this stuff as it is
 	date_default_timezone_set('America/Toronto');
 	include 'GIFEncoder.class.php';
 	include 'php52-fix.php';
+
+	// Query string variables
 	$time = $_GET['time'];
 	$color = $_GET['color'];
-	list($red, $green, $blue) = explode(",", $color);
 	$bg = $_GET['bg'];
+	$fontname = __DIR__ . DIRECTORY_SEPARATOR . fonts . DIRECTORY_SEPARATOR . $_GET['fontname'];
+	$fontsize = $_GET['fontsize'];
+
+	list($red, $green, $blue) = explode(",", $color);
 	list($bgred, $bggreen, $bgblue) = explode(",", $bg);
-	$width = $_GET['width'];
-	$height = $_GET['height'];
+
 	$future_date = new DateTime(date('r',strtotime($time)));
 	$time_now = time();
 	$now = new DateTime(date('r', $time_now));
+
 	$frames = array();	
 	$delays = array();
 
+	$interval = date_diff($future_date, $now);
 
-	// Your image link
-	$image = imagecreatetruecolor($width, $height);
-	$background = imagecolorallocate($image, $bgred, $bggreen, $bgblue);
-	imagefill($image, 0, 0, $background);
+	if($future_date < $now){
+		$text = $interval->format('00 00 00 00');
+	} else {
+		$text = $interval->format('%a %H %I %S');
+	}
+
+	// Create our bounding box for the text
+	$box = imagettfbbox($fontsize, 0, $fontname, $text);
+
+	// Measure the text
+	$textwidth = abs($box[4] - $box[0]);
+	$textheight = abs($box[5] - $box[1]);
 
 	$delay = 100;// milliseconds
 
-	$font = array(
-		'size' => 30, // Font size, in pts usually.
+	$font = ['size' => $fontsize, // Font size, in pts usually.
 		'angle' => 0, // Angle of the text
-		'file' => __DIR__ . DIRECTORY_SEPARATOR . fonts . DIRECTORY_SEPARATOR . 'JLREmeric-SemiBold.ttf', // Font path
+		'x-offset' => 0, // Usually 0 to align to the left.
+		'y-offset' => $fontsize, // The vertical alignment, usually the same as the font size.
+		'file' => $fontname, // Font path
 		'color' => imagecolorallocate($image, $red, $green, $blue), // RGB Colour of the text
-	);
+		'textwidth' => $textwidth,
+		'textheight' => $textheight
+               ];
 	for($i = 0; $i <= 60; $i++){
 		
 		$interval = date_diff($future_date, $now);
 		
 		if($future_date < $now){
 			// Create the first source image and add the text.
-			$image = imagecreatetruecolor($width, $height);
+			$image = imagecreatetruecolor($font['textwidth'], $font['textheight']);
 			$background = imagecolorallocate($image, $bgred, $bggreen, $bgblue);
 			imagefill($image, 0, 0, $background);
-			//;
-			$text = $interval->format('00   00   00   00');
-			// Create our bounding box for the text
-			$box = imagettfbbox($font['size'], $font['angle'], $font['file'], $text);
-			// Measure the text
-			$textwidth = abs($box[4] - $box[0]);
-			// Center the text
-			$x = ceil(($width - $textwidth) / 2); // lower left X coordinate for text
-			
-			imagettftext($image, $font['size'], $font['angle'], $x, $font['size'], $font['color'], $font['file'], $text);
+
+			$text = $interval->format('00 00 00 00');
+
+			imagettftext($image, $font['size'], $font['angle'], $font['x-offset'], $font['y-offset'], $font['color'], $font['file'], $text);
 			
 			ob_start();
 			imagecolortransparent($image, $background);
@@ -61,25 +71,19 @@
 			break;
 		} else {
 			// Create the first source image and add the text.
-			$image = imagecreatetruecolor($width, $height);
+			$image = imagecreatetruecolor($font['textwidth'], $font['textheight']);
 			$background = imagecolorallocate($image, $bgred, $bggreen, $bgblue);
 			imagefill($image, 0, 0, $background);
 			//;
-			$text = $interval->format('%a   %H   %I   %S');
+			$text = $interval->format('%a %H %I %S');
 			// %a is weird in that it doesn’t give you a two digit number
 			// check if it starts with a single digit 0-9
 			// and prepend a 0 if it does
 			if(preg_match('/^[0-9]\:/', $text)){
 				$text = '0'.$text;
 			}
-			// Create our bounding box for the text
-			$box = imagettfbbox($font['size'], $font['angle'], $font['file'], $text);
-			// Measure the text
-			$textwidth = abs($box[4] - $box[0]);
-			// Center the text
-			$x = ceil(($width - $textwidth) / 2); // lower left X coordinate for text
-			
-			imagettftext($image, $font['size'], $font['angle'], $x, $font['size'], $font['color'], $font['file'], $text);
+
+			imagettftext($image, $font['size'], $font['angle'], $font['x-offset'], $font['y-offset'], $font['color'], $font['file'], $text);
 
 			ob_start();
 			imagecolortransparent($image, $background);
