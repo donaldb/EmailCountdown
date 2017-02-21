dcd9e56b18da2449e7d7969fec18d411c412be7b
Don Braithwaite <don.braithwaite@outlook.com>
2017-02-21 10:36:02 -0500

    updated query string variables and added frames option

--- README.md
+++ README.md
@@ -6,13 +6,14 @@ This only simulates a countdown clock in that it displays 60 frames and then it 
 ## URL Parameters (*required)
 
 * **time*** - Date & time when your countdown will end [e.g. 2017-12-31+23:59:59]
-* **color** - hex colour code for the text [default = 000000 (black)]
-* **bg** - hex colour code for the background [default = ffffff (white)]
-* **fontname** - file name for the font to be used. The ttf file must be loaded into the /fonts folder [default = arial.ttf]
-* **fontsize** - the point size of the font [default = 30]
+* **color** - Hex colour code for the text [default = 000000 (black)]
+* **bg** - Hex colour code for the background [default = ffffff (white)]
+* **fontname** - File name for the font to be used. The ttf file must be loaded into the /fonts folder [default = arial.ttf]
+* **fontsize** - Point size of the font [default = 30]
+* **frames** - Number of frames in the animation. Must be in the range between 60-120 [default = 60]
 
 ## Example
 
-```<img src="http://server.com/countdown/gif.php?time=2017-12-31+23:59:59&color=ff0000&bg=000000&fontname=OpenSans-Regular-webfont.ttf&fontsize=30" alt="Countdown">```
+```<img src="http://server.com/countdown/gif.php?time=2017-12-31+23:59:59&color=ff0000&bg=000000&fontname=OpenSans-Regular-webfont.ttf&fontsize=30&frames=120" alt="Countdown">```
 
 [Demo](http://mustardsalt.com/countdown/index.html)
--- gif.php
+++ gif.php
@@ -1,26 +1,29 @@
 <?php
-	date_default_timezone_set('America/Toronto');
-	include 'GIFEncoder.class.php';
-	include 'hex2rgb.php';
+	date_default_timezone_set("America/Toronto");
+	include "GIFEncoder.class.php";
+	include "hex2rgb.php";
 	
 	// Query string variables
-	$time = filter_input(INPUT_GET, 'time');
-	$color = $_GET['color'] ? $_GET['color'] : '000000';
-	$bg = $_GET['bg'] ? $_GET['bg'] : 'ffffff';
-	$fontsize = $_GET['fontsize'] ? $_GET['fontsize'] : '30';
-	$fontfile = $_GET['fontname'] ? $_GET['fontname'] : 'arial.ttf';
+	$time = filter_input(INPUT_GET, "time");
+	$hexcolor = filter_input(INPUT_GET, "color") ? filter_input(INPUT_GET, "color") : "000000";
+	$hexbg = filter_input(INPUT_GET, "bg") ? filter_input(INPUT_GET, "bg") : "ffffff";
+	$fontsize = filter_input(INPUT_GET, "fontsize") ? filter_input(INPUT_GET, "fontsize") : "30";
+	$fontfile = filter_input(INPUT_GET, "fontname") ? filter_input(INPUT_GET, "fontname") : "arial.ttf";
+	
+	$options=array('options'=>array('default'=>60, 'min_range'=>60, 'max_range'=>120));
+	$framecount=filter_input(INPUT_GET, 'frames', FILTER_VALIDATE_INT, $options);
 	
 	$fontname = __DIR__ . DIRECTORY_SEPARATOR . fonts . DIRECTORY_SEPARATOR . $fontfile;
 
-	$color = hex2RGB($color, true, ',');
-	$bg = hex2RGB($bg, true, ',');
+	$color = hex2RGB($hexcolor, true, ",");
+	$bg = hex2RGB($hexbg, true, ",");
 	
 	list($red, $green, $blue) = explode(",", $color);
 	list($bgred, $bggreen, $bgblue) = explode(",", $bg);
 
-	$future_date = new DateTime(date('r',strtotime($time)));
+	$future_date = new DateTime(date("r",strtotime($time)));
 	$time_now = time();
-	$now = new DateTime(date('r', $time_now));
+	$now = new DateTime(date("r", $time_now));
 
 	$frames = array();	
 	$delays = array();
@@ -28,11 +31,11 @@
 	$interval = date_diff($future_date, $now);
 
 	if($future_date < $now){
-            $text = $interval->format('00 00 00 00');
+            $text = $interval->format("00 00 00 00");
 	} else {
-            $text = $interval->format('%a %H %I %S');
-            if(preg_match('/^[0-9]\s/', $text)){
-		$text = '0'.$text;
+            $text = $interval->format("%a %H %I %S");
+            if(preg_match("/^[0-9]\s/", $text)){
+		$text = "0".$text;
             }
 	}
 	
@@ -46,32 +49,34 @@
 	$delay = 100;// milliseconds
 	
 	$image = imagecreatetruecolor($textwidth, $textheight);
+	$textcolor = imagecolorallocate($image, $red, $green, $blue);
 	
-	$font = ['size' => $fontsize, // Font size, in pts usually.
-		'angle' => 0, // Angle of the text
-		'x-offset' => 0, // Usually 0 to align to the left.
-		'y-offset' => $textheight, // The vertical alignment, usually the same as the font size.
-		'file' => $fontname, // Font path
-		'color' => imagecolorallocate($image, $red, $green, $blue), // RGB Colour of the text
-		'textwidth' => $textwidth,
-		'textheight' => $textheight
+	$font = [
+		"size" => $fontsize, // Font size, in pts usually.
+		"angle" => 0, // Angle of the text
+		"x-offset" => 0, // Usually 0 to align to the left.
+		"y-offset" => $textheight, // The vertical alignment, usually the same as the font size.
+		"file" => $fontname, // Font path
+		"color" => $textcolor, // RGB Colour of the text
+		"textwidth" => $textwidth,
+		"textheight" => $textheight,
 	];
-	
+
 	imagedestroy($image);
 	
-	for($i = 0; $i <= 60; $i++){
+	for($i = 0; $i <= $framecount; $i++){
 		
 		$interval = date_diff($future_date, $now);
 		
 		if($future_date < $now){
 			// Create the first source image and add the text.
-			$image = imagecreatetruecolor($font['textwidth'], $font['textheight']);
+			$image = imagecreatetruecolor($font["textwidth"], $font["textheight"]);
 			$background = imagecolorallocate($image, $bgred, $bggreen, $bgblue);
 			imagefill($image, 0, 0, $background);
 
-			$text = $interval->format('00 00 00 00');
+			$text = $interval->format("00 00 00 00");
 
-			imagettftext($image, $font['size'], $font['angle'], $font['x-offset'], $font['y-offset'], $font['color'], $font['file'], $text);
+			imagettftext($image, $font["size"], $font["angle"], $font["x-offset"], $font["y-offset"], $font["color"], $font["file"], $text);
 			
 			ob_start();
 			imagecolortransparent($image, $background);
@@ -83,19 +88,19 @@
 			break;
 		} else {
 			// Create the first source image and add the text.
-			$image = imagecreatetruecolor($font['textwidth'], $font['textheight']);
+			$image = imagecreatetruecolor($font["textwidth"], $font["textheight"]);
 			$background = imagecolorallocate($image, $bgred, $bggreen, $bgblue);
 			imagefill($image, 0, 0, $background);
 
-			$text = $interval->format('%a %H %I %S');
+			$text = $interval->format("%a %H %I %S");
 			// %a is weird in that it doesn't give you a two digit number
 			// check if it starts with a single digit 0-9
 			// and prepend a 0 if it does
-			if(preg_match('/^[0-9]\s/', $text)){
-				$text = '0'.$text;
+			if(preg_match("/^[0-9]\s/", $text)){
+				$text = "0".$text;
 			}
 
-			imagettftext($image, $font['size'], $font['angle'], $font['x-offset'], $font['y-offset'], $font['color'], $font['file'], $text);
+			imagettftext($image, $font["size"], $font["angle"], $font["x-offset"], $font["y-offset"], $font["color"], $font["file"], $text);
 
 			ob_start();
 			imagecolortransparent($image, $background);
@@ -106,14 +111,14 @@
 			ob_end_clean();
 		}
 
-		$now->modify('+1 second');
+		$now->modify("+1 second");
 	}
 
 	//expire this image instantly
-	header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' );
-	header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
-	header( 'Cache-Control: no-store, no-cache, must-revalidate' );
-	header( 'Cache-Control: post-check=0, pre-check=0', false );
-	header( 'Pragma: no-cache' );
+	header( "Expires: Sat, 26 Jul 1997 05:00:00 GMT" );
+	header( "Last-Modified: " . gmdate( "D, d M Y H:i:s" ) . " GMT" );
+	header( "Cache-Control: no-store, no-cache, must-revalidate" );
+	header( "Cache-Control: post-check=0, pre-check=0", false );
+	header( "Pragma: no-cache" );
 	$gif = new AnimatedGif($frames,$delays,$loops);
 	$gif->display();
